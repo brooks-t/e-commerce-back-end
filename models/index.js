1a7c5703a4ae98549bb480cb4080ce3ecea83192
Brooks Tiffany <brooks.t.tiffany@gmail.com>
2021-04-29 15:36:51 -0700

    set up associations in models index.js

--- models/index.js
+++ models/index.js
@@ -5,12 +5,27 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
+Product.hasOne(Category);
 
 // Categories have many Products
+Category.hasMany(Product);
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {
+  through: {ProductTag,
+  unique: false
+  },
+  as: 'products_of_tags'
+});
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  through: {
+    model: ProductTag,
+    unique: false
+  },
+  as: 'tags_of_products'
+});
 
 module.exports = {
   Product,
