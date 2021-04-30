0e03fa82e019f146a2740e18794e49c77463f366
Brooks Tiffany <brooks.t.tiffany@gmail.com>
2021-04-29 17:58:55 -0700

    added an update tag route

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -33,8 +33,18 @@ router.post('/', async (req, res) => {
   }
 });
 
-router.put('/:id', (req, res) => {
-  // update a tag's name by its `id` value
+// update a tag's name by its `id` value
+router.put('/:id', async (req, res) => {
+  try {
+    const tagData = await Tag.update(req.body, {
+      where: {
+        id: req.params.id
+      }
+    });
+    res.status(200).json(tagData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.delete('/:id', (req, res) => {
