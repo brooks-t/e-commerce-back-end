95bfbb4b446db6a7d07b10aa5ea5e614c8cbce89
Brooks Tiffany <brooks.t.tiffany@gmail.com>
2021-04-29 17:14:13 -0700

    added product delete route

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -105,7 +105,17 @@ router.put('/:id', (req, res) => {
     });
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
+  try {
+    const productData = await Product.destroy({
+      where: {
+        id: req.params.id
+      }
+    });
+    res.status(200).json(productData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
   // delete one product by its `id` value
 });
 
